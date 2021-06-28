faf270b163565738c222fc071683886871749bbc
Izarothy <izarothmail@gmail.com>
2021-06-28 14:39:44 +0200

    Navbar links + deleted a useless image

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -8,17 +8,17 @@ export default function NavBar() {
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
                 </svg>
-                <a href="#" className="text-xl">Main page</a>
+                <a href="/" className="text-xl">Main page</a>
             </div>
             <div className="flex ">
             <div className="flex my-4 mx-8">
-                <a href="#" className="hover:text-teal">Discord</a>
+                <a href="https://discord.gg/hPuJf9vzkn" className="hover:text-teal">Discord</a>
             </div>
             <div className="flex my-4 mx-8">
-                <a href="#" className="hover:text-teal">Github</a>
+                <a href="https://github.com/Izarothy/meme-library" className="hover:text-teal">Github</a>
             </div>
             <div className="flex my-4 mx-8">
-                <a href="#" className="hover:text-teal">LinkedIn</a>
+                <a href="https://www.linkedin.com/in/izaroth-izaroth-870323185/" className="hover:text-teal">LinkedIn</a>
             </div>
             </div>
         </div>
